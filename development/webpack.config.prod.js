1be3c3c08fe805661ad3e4d08ec5ef3ef0dfbcb1
peteclark-io <pete.david.clark@gmail.com>
2016-10-01 18:39:08 +0100

    Adding new stuff

--- development/webpack.config.prod.js
+++ development/webpack.config.prod.js
@@ -3,7 +3,7 @@ var webpack = require('webpack');
 module.exports = {
     entry: "./app/main.js",
     output: {
-        path: __dirname + '/dist/js',
+        path: __dirname + '/../public/js',
         filename: "bundle.js"
     },
     module: {
@@ -17,12 +17,30 @@ module.exports = {
                 query: {
                     presets: ['es2015']
                 }
+            }, {
+              test: /\.css$/,
+              loader: 'style-loader'
+            }, {
+                test: /\.css$/,
+                loader: 'css-loader',
+                query: {
+                  sourceMap: false,
+                  modules: true,
+                  localIdentName: '[hash:base64:4]',
+                  minimize: true,
+                }
+            }, {
+              test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)(\?.*)?$/,
+              loader: 'url-loader?limit=50000'
             }
         ]
     },
     plugins: [
       new webpack.optimize.DedupePlugin(),
-      new webpack.optimize.UglifyJsPlugin(),
+      new webpack.optimize.UglifyJsPlugin({
+        compress: true,
+        comments: false
+      }),
       new webpack.optimize.AggressiveMergingPlugin()
     ]
 };
