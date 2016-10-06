3c24d7594f5c17a3f76d71229e5d9923450936d3
Clark, Peter <pete.david.clark@gmail.com>
2016-10-06 16:02:07 +0100

    Adding charts

--- development/app/components/Matches/Questions/QuestionGroup.js
+++ development/app/components/Matches/Questions/QuestionGroup.js
@@ -6,7 +6,7 @@ import bootstrap from 'bootstrap/dist/css/bootstrap.css';
 
 import styles from './styles.css';
 
-const Question = React.createClass({
+const QuestionGroup = React.createClass({
 
    propTypes: {
       question: React.PropTypes.string,
@@ -22,16 +22,10 @@ const Question = React.createClass({
                <div className={classNames(bootstrap['col-xs-12'], bootstrap['col-md-2'])}>
                   <h3><small>{this.props.time}</small></h3>
                </div>
-               <div className={classNames(bootstrap['col-xs-12'], bootstrap['col-md-10'])}>
-                  <h3>{this.props.question}</h3>
-                  <div className={styles.spacer}></div>
-                  <a className={classNames(styles['action-button'], styles.green, styles.animate)}>Yes</a>
-                  <a className={classNames(styles['action-button'], styles.red, styles.animate)}>No</a>
-               </div>
             </div>
          </div>
       );
    }
 });
 
-export default Question;
+export default QuestionGroup;
