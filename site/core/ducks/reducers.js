dafe35f843d378ca5ce1d5971bcd4397e9eca14a
peteclark-io <pete.david.clark@gmail.com>
2016-10-10 22:13:27 +0100

    Deleting old site

--- site/core/ducks/reducers.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from 'redux';
-import matches from './matches';
-
-const rootReducer = matches;
-
-export default rootReducer;
--- site/core/history.js
+++
@@ -1,6 +0,0 @@
-'use strict'
-
-import createBrowserHistory from 'history/lib/createBrowserHistory';
-import useQueries from 'history/lib/useQueries';
-
-export default useQueries(createBrowserHistory)();
