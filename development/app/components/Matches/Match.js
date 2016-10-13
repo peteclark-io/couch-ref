48ed69271041319f0f9813bafe3a8d4ea15486a6
peteclark-io <pete.david.clark@gmail.com>
2016-10-13 23:33:52 +0100

    Fixing styles a bit

--- development/app/components/Matches/Match.js
+++ development/app/components/Matches/Match.js
@@ -50,7 +50,7 @@ const Match = React.createClass({
                   <h2 className={styles['match-header']}>{this.props.match.home} {this.props.match.goalsHome}  -  {this.props.match.goalsAway} {this.props.match.away}</h2>
                </div>
                <div className={classNames(bootstrap['col-xs-12'], bootstrap['col-sm-4'])}>
-                  <Link className={styles.link} to={`/teams/${this.props.match.id}`}><h2 className={styles['match-header']}><small>Teams</small></h2></Link>
+                  <Link className={styles.link} to={`/teams/${this.props.match.id}`}><h2 className={styles['teams-link']}><small>Teams</small></h2></Link>
                </div>
             </div>
             <QuestionList questions={this.props.match.questions} />
