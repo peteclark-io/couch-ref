5e16a137217604cb431685afe0786d5c3fd81890
Clark, Peter <peter.clark@ft.com>
2016-11-15 09:32:36 +0000

    Removing cli

--- tools/cli/verdict/verdict.go
+++
@@ -1,8 +0,0 @@
-package verdict
-
-import "github.com/peteclark-io/couch-ref/tools/cli/structs"
-
-func Verdict(fixture structs.Fixture, results structs.Match) (bool, float64) {
-
-	return true, 0
-}
