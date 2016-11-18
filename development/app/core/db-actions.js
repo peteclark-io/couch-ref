52614cb611e6b8432998992d35c7412ed30403d4
Clark, Peter <peter.clark@ft.com>
2016-11-18 15:35:17 +0000

    Saving a users vote against their profile

--- development/app/core/db-actions.js
+++ development/app/core/db-actions.js
@@ -4,173 +4,187 @@ import firebase from 'firebase';
 import moment from 'moment';
 
 export const saveVote = (user, question, vote) => {
-  var database = firebase.database();
-  var stats = database.ref('/v0/live-statistics/' + question.id);
+   var database = firebase.database();
+   var stats = database.ref('/v0/live-statistics/' + question.id);
 
-  stats.transaction((stat) => {
-    console.log(user, stat);
-    var simple = stat.simple
-    vote ? simple.yes++ : simple.no++;
+   stats.transaction((stat) => {
+      console.log(user, stat);
+      var simple = stat.simple
+      vote ? simple.yes++ : simple.no++;
 
-    var breakdown = stat.breakdown;
-    if (!breakdown){
-      breakdown = initialise();
-    }
+      var breakdown = stat.breakdown;
+      if (!breakdown){
+         breakdown = initialise();
+      }
 
-    generateClubBreakdown(user, vote, breakdown);
-    generateAgeBreakdown(user, vote, breakdown);
-    generateLocationBreakdown(user, vote, breakdown);
+      generateClubBreakdown(user, vote, breakdown);
+      generateAgeBreakdown(user, vote, breakdown);
+      generateLocationBreakdown(user, vote, breakdown);
 
-    if (user.sex){
-      vote ? breakdown.sex[user.sex].yes++ : breakdown.sex[user.sex].no++;
-    }
+      if (user.sex){
+         vote ? breakdown.sex[user.sex].yes++ : breakdown.sex[user.sex].no++;
+      }
 
-    stat.breakdown = breakdown;
-    return stat;
-  });
+      stat.breakdown = breakdown;
+      return stat;
+   });
+
+   if (!user.remote.uid){
+      return;
+   }
+
+   var usersVotes = database.ref('/v0/users-votes/' + user.remote.uid);
+   usersVotes.transaction((userVote) => {
+      console.log(userVote);
+      if (!userVote){
+         userVote = {};
+      }
+      userVote[question.id] = vote ? 'Yes' : 'No';
+      return userVote;
+   });
 };
 
 const zeroValue = () => {
-  return {yes: 0, no: 0};
+   return {yes: 0, no: 0};
 };
 
 const initialise = () => {
-  return {
-    club: {},
-    age: {},
-    sex: {
-      Male: zeroValue(),
-      Female: zeroValue(),
-      Other: zeroValue()
-    },
-    location: {}
-  };
+   return {
+      club: {},
+      age: {},
+      sex: {
+         Male: zeroValue(),
+         Female: zeroValue(),
+         Other: zeroValue()
+      },
+      location: {}
+   };
 };
 
 const ageGroup = (age) => {
-  if(age <= 20) {
-    return '< 20';
-  }
+   if(age <= 20) {
+      return '< 20';
+   }
 
-  if(age <= 30) {
-    return '21 - 30';
-  }
+   if(age <= 30) {
+      return '21 - 30';
+   }
 
-  if(age <= 40) {
-    return '31 - 40';
-  }
+   if(age <= 40) {
+      return '31 - 40';
+   }
 
-  if(age <= 50) {
-    return '41 - 50';
-  }
+   if(age <= 50) {
+      return '41 - 50';
+   }
 
-  if(age <= 60) {
-    return '51 - 60';
-  }
+   if(age <= 60) {
+      return '51 - 60';
+   }
 
-  return '60+';
+   return '60+';
 };
 
 const generateClubBreakdown = (user, vote, breakdown) => {
-  if (!user.club || !user.club.shortName){
-    return;
-  }
+   if (!user.club || !user.club.shortName){
+      return;
+   }
 
-  if (!breakdown.club[user.club.shortName]){
-    breakdown.club[user.club.shortName] = zeroValue();
-  }
+   if (!breakdown.club[user.club.shortName]){
+      breakdown.club[user.club.shortName] = zeroValue();
+   }
 
-  vote ? breakdown.club[user.club.shortName].yes++ : breakdown.club[user.club.shortName].no++;
+   vote ? breakdown.club[user.club.shortName].yes++ : breakdown.club[user.club.shortName].no++;
 };
 
 const generateLocationBreakdown = (user, vote, breakdown) => {
-  if (!user.location){
-    return;
-  }
+   if (!user.location){
+      return;
+   }
 
-  if (!breakdown.location){
-    breakdown.location = {[user.location]: zeroValue()};
-  }
+   if (!breakdown.location){
+      breakdown.location = {[user.location]: zeroValue()};
+   }
 
-  if (!breakdown.location[user.location]){
-    breakdown.location[user.location] = zeroValue();
-  }
+   if (!breakdown.location[user.location]){
+      breakdown.location[user.location] = zeroValue();
+   }
 
-  vote ? breakdown.location[user.location].yes++ : breakdown.location[user.location].no++;
+   vote ? breakdown.location[user.location].yes++ : breakdown.location[user.location].no++;
 };
 
 const generateAgeBreakdown = (user, vote, breakdown) => {
-  if (!user.birthday){
-    return;
-  }
+   if (!user.birthday){
+      return;
+   }
 
-  var age = moment().diff(user.birthday, 'years');
-  console.log(user.birthday, age);
-  var group = ageGroup(age);
+   var age = moment().diff(user.birthday, 'years');
+   console.log(user.birthday, age);
+   var group = ageGroup(age);
 
-  if (!breakdown.age){
-    breakdown.age = {[group]: zeroValue()};
-  }
+   if (!breakdown.age){
+      breakdown.age = {[group]: zeroValue()};
+   }
 
-  if (!breakdown.age[group]){
-    breakdown.age[group] = zeroValue();
-  }
+   if (!breakdown.age[group]){
+      breakdown.age[group] = zeroValue();
+   }
 
-  vote ? breakdown.age[group].yes++ : breakdown.age[group].no++;
+   vote ? breakdown.age[group].yes++ : breakdown.age[group].no++;
 };
 
 export const saveClub = (user, club) => {
-  var database = firebase.database();
-  var users = database.ref('/v0/users/' + user.remote.uid);
+   var database = firebase.database();
+   var users = database.ref('/v0/users/' + user.remote.uid);
 
-  users.transaction((u) => {
-    console.log(user, u);
-    if (!u){
-      u = {};
-    }
-    u.club = club.name;
-    return u;
-  });
+   users.transaction((u) => {
+      console.log(user, u);
+      if (!u){
+         u = {};
+      }
+      u.club = club.name;
+      return u;
+   });
 };
 
 export const saveDateOfBirth = (user, birthday) => {
-  var database = firebase.database();
-  var users = database.ref('/v0/users/' + user.remote.uid);
+   var database = firebase.database();
+   var users = database.ref('/v0/users/' + user.remote.uid);
 
-  users.transaction((u) => {
-    console.log(user, u);
-    if (!u){
-      u = {};
-    }
-    u.birthday = birthday.toISOString();
-    return u;
-  });
+   users.transaction((u) => {
+      console.log(user, u);
+      if (!u){
+         u = {};
+      }
+      u.birthday = birthday.toISOString();
+      return u;
+   });
 };
 
 export const saveSex = (user, sex) => {
-  var database = firebase.database();
-  var users = database.ref('/v0/users/' + user.remote.uid);
+   var database = firebase.database();
+   var users = database.ref('/v0/users/' + user.remote.uid);
 
-  users.transaction((u) => {
-    console.log(user, u);
-    if (!u){
-      u = {};
-    }
-    u.sex = sex;
-    return u;
-  });
+   users.transaction((u) => {
+      console.log(user, u);
+      if (!u){
+         u = {};
+      }
+      u.sex = sex;
+      return u;
+   });
 };
 
 export const saveLocation = (user, location) => {
-  var database = firebase.database();
-  var users = database.ref('/v0/users/' + user.remote.uid);
+   var database = firebase.database();
+   var users = database.ref('/v0/users/' + user.remote.uid);
 
-  users.transaction((u) => {
-    console.log(user, u);
-    if (!u){
-      u = {};
-    }
-    u.location = location;
-    return u;
-  });
+   users.transaction((u) => {
+      console.log(user, u);
+      if (!u){
+         u = {};
+      }
+      u.location = location;
+      return u;
+   });
 };
