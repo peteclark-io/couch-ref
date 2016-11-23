a7ff480f10214fe394c018bab7ed0bc621ddb715
Clark, Peter <peter.clark@ft.com>
2016-11-23 12:19:25 +0000

    Scores now work

--- tools/scores/scores/cmd.js
+++ tools/scores/scores/cmd.js
@@ -2,33 +2,118 @@
 
 const admin = require('firebase-admin');
 const co = require('co');
-const getStdin = require('get-stdin');
+const _ = require('lodash');
+const magic = require('./magic');
+const exec = require('child_process').exec;
 
 admin.initializeApp({
    credential: admin.credential.cert('./couchref-9962e-firebase-adminsdk.json'),
    databaseURL: "https://couchref-9962e.firebaseio.com"
 });
 
-exports.command = 'scores <matchId>';
+const getFromDb = (db) => {
+   return new Promise((resolve, reject) => {
+      db.once('value', (snapshot) => {
+         resolve(snapshot.val());
+      });
+   });
+};
+
+const getMatch = (id, matches) => {
+   return matches.filter((m) => {return m.id === id})[0];
+};
+
+const generateScore = (vote, verdict) => {
+   return new Promise((resolve, reject) => {
+      var cmd = 'scores --confidence "' + verdict.confidence + '" --result "' + verdict.percentage + '" --vote "' + vote + '"'
+      exec(cmd, function(error, stdout, stderr) {
+         var score = JSON.parse(stdout).score;
+         resolve(score);
+      });
+   });
+};
+
+const saveScore = (db, questionDb, user) => {
+   questionDb.transaction((q) => {
+      if (!q){
+         return q;
+      }
+
+      q.scored = true;
+      return q;
+   });
+
+   db.ref('/v0/users/' + user.uid).transaction((u) => {
+      if (!u) {
+         return u;
+      }
+
+      var score = u.score;
+      if (!score){
+         score = 2000;
+      }
+
+      score = score + user.score;
+      u.score = score;
+      return u;
+   });
+};
+
+exports.command = 'scores';
 exports.describe = '';
 exports.handler = (argv) => {
    //admin.database.enableLogging(true);
    var db = admin.database();
-   var questions = db.ref('/v0/live-matches/' + argv.matchId + '/questions');
-   
 
    co(function *(){
-      var qs = yield getQuestions();
-      questions.off();
+      var matches = yield getFromDb(db.ref('/v0/live-matches/'));
+      var readyToScore = Object.keys(matches).map((uuid) => {
+         return matches[uuid];
+      }).filter((match) => {return match.full_time;});
 
-      var stdin = yield getStdin();
-      try {
-        var users = JSON.parse(stdin);
-        qs.map((q) => {
-          answerQuestion(q, users);
-        });
-      } catch(err){
-        console.log(err);
+      var questionUuids = _.flatten(readyToScore.map((match) => {
+         return match.questions.map((q) => {return q.id;});
+      }));
+
+      var usersDb = yield getFromDb(db.ref('/v0/users'));
+      var usersVotes = yield getFromDb(db.ref('/v0/users-votes'));
+
+      var uidsForVoters = Object.keys(usersVotes);
+
+      for (var i = 0 ; i < questionUuids.length ; i++){
+         var questionDb = db.ref('/v0/live-questions/' + questionUuids[i]);
+         var question = yield getFromDb(questionDb);
+         if (!question.decision || question.decision.trim() === '' || question.scored){
+            continue;
+         }
+
+         var stats = yield getFromDb(db.ref('/v0/live-statistics/' + questionUuids[i]));
+         var verdict = magic.verdict(getMatch(question.match, readyToScore), stats);
+
+         var users = uidsForVoters.map((uid) => {
+            if (usersVotes[uid][questionUuids[i]]){
+               return {
+                  uid: uid,
+                  user: usersDb[uid],
+                  vote: usersVotes[uid][questionUuids[i]]
+               };
+            }
+            return undefined;
+         });
+
+         for (var j = 0 ; j < users.length ; j++){
+            var vote = users[j].vote === 'Yes' ? true : false;
+            var score = yield generateScore(vote, verdict);
+            users[j].score = score;
+         }
+
+         users.map((user) => {
+            saveScore(db, questionDb, user);
+         });
       }
+   }).catch((err) => {
+      console.log(err);
+   }).then(() => {
+      console.log('k, thanks, bye.');
    });
 };
---
+++ tools/scores/scores/magic.js
@@ -0,0 +1,134 @@
+'use strict';
+
+const BASIC = 'core/magic/BASIC';
+const COMPLEX = 'core/magic/COMPLEX';
+const populationRequirement = 100;
+
+const basicVerdict = (match, results) => {
+  if (!results.simple){
+    return undefined;
+  }
+
+  return Object.assign({
+    verdict: decideVerdict(perc.percentage),
+    type: BASIC,
+  }, producePercentage(results.simple));
+};
+
+exports.verdict = (match, results) => {
+  //console.log('Generating verdict.', match, results);
+  if (!results.breakdown){
+    return basicVerdict(match, results);
+  }
+
+  var neutral = {yes: results.simple.yes, no: results.simple.no};
+
+  var home = undefined;
+  if (results.breakdown.club[match.home]){
+    home = {
+      yes: results.breakdown.club[match.home].yes,
+      no: results.breakdown.club[match.home].no
+    };
+    neutral.yes = neutral.yes - home.yes;
+    neutral.no = neutral.no - home.no;
+  }
+
+  var away = undefined;
+  if (results.breakdown.club[match.away]){
+    away = {
+      yes: results.breakdown.club[match.away].yes,
+      no: results.breakdown.club[match.away].no
+    };
+    neutral.yes = neutral.yes - away.yes;
+    neutral.no = neutral.no - away.no;
+  }
+
+  //console.log(home, away, neutral);
+  var homePerc = producePercentage(home);
+  var awayPerc = producePercentage(away);
+  var neutralPerc = producePercentage(neutral);
+
+  var avg = produceAverage(homePerc.percentage, awayPerc.percentage, neutralPerc.percentage);
+  var confidence = produceAverage(homePerc.confidence, awayPerc.confidence, neutralPerc.confidence);
+
+  return {
+    verdict: decideVerdict(avg),
+    type: COMPLEX,
+    percentage: avg,
+    home: homePerc,
+    away: awayPerc,
+    neutral: neutralPerc,
+    confidence: confidence
+  };
+};
+
+const producePercentage = (simple = {yes: 0, no: 0}) => {
+  if (simple.yes === 0 && simple.no === 0){
+    return {
+      percentage: 0.5,
+      confidence: 0
+    };
+  }
+
+  var total = simple.yes + simple.no;
+  return {
+    percentage: simple.yes > 0 ? simple.yes / total : 0,
+    confidence: produceConfidence(total)
+  };
+};
+
+const produceConfidence = (population) => {
+  return population > populationRequirement ? 1 : population / populationRequirement;
+}
+
+const produceAverage = (home, away, neutral) => {
+  return (home + away + neutral) / 3;
+};
+
+const decideVerdict = (percentage) => {
+  if (percentage > 0.8){
+    return 'Resounding Yes!';
+  }
+
+  if (percentage < 0.495 && percentage >= 0.47){
+    return 'Close, but No!';
+  }
+
+  if (percentage < 0.47 && percentage >= 0.2){
+    return 'No!';
+  }
+
+  if (percentage < 0.2){
+    return 'Resounding No!';
+  }
+
+  if(percentage < 0.505 && percentage > 0.495){
+    return 'Too Close to Call!';
+  }
+
+  if (percentage < 0.53 && percentage >= 0.505){
+    return 'Close, but Yes!';
+  }
+
+  return 'Yes!';
+};
+
+const decideConfidence = (confidence) => {
+  if (confidence === 1){
+    return 'Very Confident';
+  }
+
+  if (confidence > 0.8){
+    return 'Confident';
+  }
+
+  if (confidence > 0.4){
+    return 'Fairly Confident';
+  }
+
+  if (confidence > 0.1){
+    return 'Slightly Confident';
+  }
+
+  return 'Not Confident';
+};
