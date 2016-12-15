e44a89270c7ca56786539ae1a3a1a23598ec8aa7
Clark, Peter <peter.clark@ft.com>
2016-12-15 16:44:37 +0000

    Adding wip back button

--- development/app/pages/banners/UsersPage.js
+++ development/app/pages/banners/UsersPage.js
@@ -2,6 +2,7 @@
 
 import React from 'react';
 import {connect} from 'react-redux';
+import {Link} from 'react-router';
 
 import styles from './UsersPage.css';
 
@@ -9,8 +10,13 @@ const UsersPage = React.createClass({
 
   render: function() {
     return (
-      <div className={styles.splash}>
-         {this.props.children}
+      <div>
+         <div className={styles.splash}>
+            {/*<div className={styles.back}>
+               <Link to={'/'}><h1>Back</h1></Link>
+            </div>*/}
+            {this.props.children}
+         </div>
       </div>
     );
   }
