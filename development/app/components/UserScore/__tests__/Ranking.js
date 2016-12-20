add048fe3cad5c36f33423c9723dc58f2b44faf2
peteclark-io <pete.david.clark@gmail.com>
2016-12-20 20:58:07 +0000

    Fixed test

--- development/app/components/UserScore/__tests__/Ranking.js
+++ development/app/components/UserScore/__tests__/Ranking.js
@@ -1,7 +1,7 @@
 'use strict';
 
 import React from 'react';
-import {Ranking} from '../Ranking';
+import Ranking from '../Ranking';
 import renderer from 'react-test-renderer';
 import {mount, shallow} from 'enzyme';
 
